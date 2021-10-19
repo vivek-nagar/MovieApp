71c966d7c049aef0bf1de876c924ee937fd436fb
Vivek Nagar <92670427+vivek-nagar@users.noreply.github.com>
2021-10-19 17:33:27 +0530

    Update SearchBox.js

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
+import { MdSearch } from "react-icons/md";
 
 const SearchBox = (props) => {
-	return (
-		<div className='col col-sm-4'>
-			<input
-				className='form-control'
-				value={props.value}
-				onChange={(event) => props.setSearchValue(event.target.value)}
-				placeholder='Type to search...'
-			></input>
-		</div>
-	);
+  return (
+    <div className="search">
+      <MdSearch className="search-icons" color="black" size="1.3em" />
+      <input
+        value={props.value}
+        onChange={(event) => props.setSearchValue(event.target.value)}
+        placeholder="Type to search..."
+      ></input>
+    </div>
+  );
 };
 
 export default SearchBox;
